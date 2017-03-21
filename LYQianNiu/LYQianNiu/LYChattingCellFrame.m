ea2e8483eb46efaf54828da3908e18e2670996f0
CoderLeonidas <1538392301@qq.com>
2017-03-21 19:06:34 +0800

    no message

--- LYQianNiu/LYQianNiu/LYChattingCellFrame.m
+++ LYQianNiu/LYQianNiu/LYChattingCellFrame.m
@@ -29,9 +29,6 @@
     _cellModel = cellModel;
     
     [self calculateFrames];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        
-    });
 }
 
 - (void)calculateFrames {
