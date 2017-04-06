42db80609eebf89e38bf2c4e32db57e91f2ae820
CoderLeonidas <1538392301@qq.com>
2017-04-07 01:20:17 +0800

    网络相关的单元测试demo

--- NetWork-Unit-Test/NetWork-Unit-TestTests/NetWork_Unit_TestTests.m
+++ NetWork-Unit-Test/NetWork-Unit-TestTests/NetWork_Unit_TestTests.m
@@ -8,6 +8,8 @@
 
 #import <XCTest/XCTest.h>
 #import <AFNetworking.h>
+#import <OCMock.h>
+#import "AFHTTPSessionManager.h"
 @interface NetWork_Unit_TestTests : XCTestCase
 
 @end
@@ -33,6 +35,26 @@
     // This is an example of a performance test case.
     [self measureBlock:^{
         // Put the code you want to measure the time of here.
+    }];
+}
+- (void)testNetwork {
+    id mockManager = [OCMockObject mockForClass:[AFHTTPSessionManager class]];
+    
+    [[[mockManager expect] andDo:^(NSInvocation *invocation) {
+        
+        void (^successBlock)(NSURLSessionDataTask *task, id responseObject) = nil;
+        
+        [invocation getArgument:&successBlock atIndex:(2+3)];
+        
+        successBlock([[NSURLSessionDataTask alloc] init], @{@"keyTest":@"valueTest"});
+        
+    }] POST:[OCMArg any] parameters:nil progress:[OCMArg any] success:[OCMArg any] failure:[OCMArg any]];
+    
+    
+    [mockManager POST:@"any path" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        XCTAssertEqual(@"valueTest", responseObject[@"keyTest"]);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
     }];
 }
 
